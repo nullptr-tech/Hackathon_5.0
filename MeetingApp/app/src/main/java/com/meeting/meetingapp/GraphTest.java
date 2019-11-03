081eecd728e9a8080cb334cff7f339bc021a8f0d
Samuel Haque <38042997+samuelhaque@users.noreply.github.com>
2019-11-03 07:09:21 +0000

    push

--- MeetingApp/app/src/main/java/com/meeting/meetingapp/GraphTest.java
+++ MeetingApp/app/src/main/java/com/meeting/meetingapp/GraphTest.java
@@ -2,12 +2,21 @@ package com.meeting.meetingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import org.eazegraph.lib.charts.ValueLineChart;
 import org.eazegraph.lib.models.ValueLinePoint;
 import org.eazegraph.lib.models.ValueLineSeries;
 
+import java.sql.Struct;
+
 public class GraphTest extends AppCompatActivity {
+
+    ImageView pig1, pig2, pig3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +24,30 @@ public class GraphTest extends AppCompatActivity {
         setContentView(R.layout.activity_graph_test);
 
 
-        ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.cubiclinechart);
+        pig1 = (ImageView) findViewById(R.id.piggie1);
+        pig2 = (ImageView) findViewById(R.id.piggie2);
+        pig3 = (ImageView) findViewById(R.id.piggie3);
 
+        pig1.setVisibility(View.INVISIBLE);
+        pig2.setVisibility(View.INVISIBLE);
+        pig3.setVisibility(View.INVISIBLE);
+
+        ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.cubiclinechart);
         ValueLineSeries series = new ValueLineSeries();
         series.setColor(0xFF56B7F1);
+
+        double moneyIn = 10.2 + 3.0+ 12.54 + 34.2;
+        double moneyOut = 31.2 + 23 + 12.54 + 10.2;
+        if ( moneyIn < moneyOut){
+            pig1.setVisibility(View.VISIBLE);
+        }else if(moneyIn > moneyOut){
+            pig1.setVisibility(View.VISIBLE);
+            pig2.setVisibility(View.VISIBLE);
+            pig3.setVisibility(View.VISIBLE);
+        }else if(moneyIn == moneyOut){
+            pig1.setVisibility(View.VISIBLE);
+            pig2.setVisibility(View.VISIBLE);
+        }
 
         series.addPoint(new ValueLinePoint("Mon", 0.0f));
         series.addPoint(new ValueLinePoint("Tue", 3.4f));
